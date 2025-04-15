0aa29cb2e9833a98ba88a3dae32fd6df95d0b79b
MarianoSanchez <mariano.sanchez@utec.edu.pe>
2025-04-14 23:11:15 -0500

    Nuevo par de claves

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
 
         System.out.printf("Este es mi primer proyect con git");
+        System.out.printf("YA PUSE LA BENDITA CLAVEEEEEEE");
 
         for (int i = 1; i <= 5; i++) {
 
