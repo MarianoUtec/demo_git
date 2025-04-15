0377e16691465b3dfeda2d4575472da148219faa
MarianoSanchez <mariano.sanchez@utec.edu.pe>
2025-04-14 22:30:46 -0500

    Un mensajito de bienvenida

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Este es mi primer proyect con git");
 
         for (int i = 1; i <= 5; i++) {
 
